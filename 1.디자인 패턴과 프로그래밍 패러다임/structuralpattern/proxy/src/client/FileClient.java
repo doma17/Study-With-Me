1aa29f76c7a875564bb52f491f5c4dcc5c4780a9
BHC-Chicken <tij05078@gmail.com>
2025-06-16 21:33:04 +0900

    [Design Pattern] 개요, 생성 패턴, 구조 패턴

---
+++ 1.디자인 패턴과 프로그래밍 패러다임/structuralpattern/proxy/src/client/FileClient.java
@@ -0,0 +1,34 @@
+package client;
+
+import javax.naming.NamingException;
+import java.io.*;
+import java.rmi.Naming;
+import java.rmi.NotBoundException;
+
+public class FileClient {
+    public static byte[] fileToBytes(File file) throws IOException {
+
+        try (FileInputStream fis = new FileInputStream(file)) {
+            byte[] fileBytes = new byte[(int) file.length()];
+            fis.read(fileBytes);
+            return fileBytes;
+        }
+    }
+
+    public static void main(String[] args) throws NamingException, IOException, NotBoundException {
+
+        FileServer fileClient = (FileServer) Naming.lookup("rmi://127.0.0.1/fileUpload");
+
+        File file = new File("/Users/phc/development/080258/ch04/exercises/image-gallery/Dockerfile");
+        byte[] fileBytes = fileToBytes(file);
+        fileClient.uploadFile(fileBytes, file.getName());
+
+        file = new File("/Users/phc/development/080258/ch08/exercises/numbers/docker-compose.yml");
+        fileBytes = fileToBytes(file);
+        fileClient.uploadFile(fileBytes, file.getName());
+
+        fileClient.sendMessage("hash", "done");
+
+        System.out.println(fileClient.fileList());
+    }
+}
